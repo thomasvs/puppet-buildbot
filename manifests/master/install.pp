3c2909374413642002a572dcc71f3d4979f5a9d8
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2015-03-20 21:23:43 +0000

    make this work on ubuntu too

--- manifests/master/install.pp
+++ manifests/master/install.pp
@@ -2,9 +2,23 @@ class buildbot::master::install {
 
   include buildbot::common::install
 
-  package {[
-    'buildbot-master',
-  ]:
+  case $::operatingsystem {
+    /^(RedHat|CentOS|Fedora)$/: {
+      $pkgs = [
+        'buildbot-master',
+      ]
+    }
+    /^(Ubuntu|Debian)$/: {
+      $pkgs = [
+        'buildbot',
+      ]
+    }
+     default: {
+      fail('Unsupported operating system')
+    }
+  }
+
+  package { $pkgs:
     ensure => present,
   }
 
